6927d881912108e67da7b0027ccbddf8f11c0349
Oscar Wellner <o.wellner@telfort.nl>
2020-12-10 22:47:48 +0100

    Step 5: Setup the Color Item RecyclerView

--- app/src/main/java/com/nonamer777/madlevel6task1/ui/ColorFragment.kt
+++ app/src/main/java/com/nonamer777/madlevel6task1/ui/ColorFragment.kt
@@ -5,15 +5,56 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.nonamer777.madlevel6task1.R
+import com.nonamer777.madlevel6task1.databinding.FragmentColorBinding
+import com.nonamer777.madlevel6task1.model.ColorItem
 
 /**
  * A [Fragment] subclass where Users can touch a color and see what that color's name is.
  */
 class ColorFragment: Fragment() {
 
+    private lateinit var binding: FragmentColorBinding
+
+    private val colors = arrayListOf<ColorItem>()
+
+    private val colorAdapter = ColorAdapter(colors, ::onColorClick)
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_color, container, false)
+    ): View {
+
+        binding = FragmentColorBinding.inflate(layoutInflater)
+
+        initializeRecyclerView()
+
+        return binding.root
+    }
+
+    private fun initializeRecyclerView() {
+        binding.colorsList.layoutManager = LinearLayoutManager(
+            activity,
+            RecyclerView.VERTICAL,
+            false
+        )
+        binding.colorsList.adapter = colorAdapter
+
+        observeColors()
+    }
+
+    private fun onColorClick(colorItem: ColorItem) {
+        Snackbar.make(
+            binding.colorsList,
+            "This color is: ${colorItem.name}",
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+
+    private fun observeColors() {
+        
+    }
 }
